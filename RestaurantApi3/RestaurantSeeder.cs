6a08f32a56367fafd101a205913cd9c75edc4758
Martyna <martyna.niegowska@motorolasolutions.com>
2024-02-03 22:07:21 +0100

    Add better logs

--- RestaurantApi3/RestaurantSeeder.cs
+++ RestaurantApi3/RestaurantSeeder.cs
@@ -24,30 +24,43 @@ public class RestaurantSeeder
             return;
         }
         
-        var pendingMigrations = _dbContext.Database.GetPendingMigrations();
-        if(pendingMigrations != null && pendingMigrations.Any()) {
-            _logger.LogInformation(AppConstants.LoggerInformationPrefix + "Performing some migrations");
-            _logger.LogDebug(AppConstants.LoggerInformationPrefix + "debug Performing some migrations");
-            _logger.LogTrace(AppConstants.LoggerInformationPrefix + "trace Performing some migrations");
-            _dbContext.Database.Migrate();
-        }
+        _logger.LogWarning(AppConstants.LoggerWarnPrefix + "Successfully connected to database");
 
-        if (!_dbContext.Roles.Any())
+        try
         {
-            var roles = GetRoles();
-            _dbContext.Roles.AddRange(roles);
-            _dbContext.SaveChanges();
-            _logger.LogInformation(AppConstants.LoggerInformationPrefix + "Successfully seeded Roles in database");
+            var pendingMigrations = _dbContext.Database.GetPendingMigrations();
+            if (pendingMigrations != null && pendingMigrations.Any())
+            {
+                _logger.LogWarning(AppConstants.LoggerWarnPrefix + "Performing some migrations");
+                _dbContext.Database.Migrate();
+            }
+
+            if (!_dbContext.Roles.Any())
+            {
+                var roles = GetRoles();
+                _dbContext.Roles.AddRange(roles);
+                _dbContext.SaveChanges();
+                _logger.LogInformation(AppConstants.LoggerInformationPrefix + "Successfully seeded Roles in database");
+            }
+
+
+            if (!_dbContext.Restaurants.Any())
+            {
+                var restaurants = getRestaurants();
+                _dbContext.Restaurants.AddRange(restaurants);
+                _dbContext.SaveChanges();
+                _logger.LogInformation(AppConstants.LoggerInformationPrefix +
+                                       "Successfully seeded Restaurants in database");
+            }
         }
-
-
-        if (!_dbContext.Restaurants.Any())
+        catch (Exception e)
         {
-            var restaurants = getRestaurants();
-            _dbContext.Restaurants.AddRange(restaurants);
-            _dbContext.SaveChanges();
-            _logger.LogInformation(AppConstants.LoggerInformationPrefix + "Successfully seeded Restaurants in database");
+            _logger.LogWarning(AppConstants.LoggerWarnPrefix + "Error when seeding database: " + e.Message);
+            _logger.LogError(AppConstants.LoggerErrorPrefix + "Error when seeding database: " + e.Message, e);
+            throw e;
         }
+        
+       
     }
 
     private IEnumerable<Restaurant> getRestaurants()
